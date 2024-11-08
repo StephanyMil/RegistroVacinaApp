6b6320decf5331d8635e2951853d781955e4dbcd
StephanyMil <stephanymilhomem25@gmail.com>
2024-11-08 06:19:21 -0300

    Melhorando a Home

--- app/index.tsx
+++ app/index.tsx
@@ -7,16 +7,24 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
+      {/* Header */}
+      <View style={styles.header}>
+        <Image source={require('../assets/images/ic_vaccine.png')} style={styles.headerImage} />
+        <Text style={styles.headerText}>Vacinação</Text>
+      </View>
+
+      {/* Calendário Button */}
       <TouchableOpacity
         style={styles.button}
         onPress={() => router.push('/calendario')}
       >
         <Image
-          source={require('../assets/images/calendario 1.png')}
+          source={require('../assets/images/calendario.png')}
           style={styles.image}
         />
-        <Text style={styles.text}>Calendário</Text>
       </TouchableOpacity>
+
+      <Text style={styles.buttonText}>Calendário</Text>
     </View>
   );
 };
@@ -24,31 +32,49 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#f0f0f0',
+    justifyContent: 'center',
+    backgroundColor: '#0066cc', // Azul para o cabeçalho
+    paddingVertical: 20, // Aumentar o padding vertical
+    marginBottom: 20, // Separação entre o cabeçalho e o botão
+    borderRadius: 10,
+  },
+  headerImage: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
   },
   button: {
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ffffff',
-    borderRadius: 10,
+    backgroundColor: '#E2EAFF',
     padding: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 3,
-    elevation: 5,
-  },
-  image: {
+    borderRadius: 8,
     width: 50,
     height: 50,
-    marginBottom: 10,
   },
-  text: {
-    fontSize: 16,
-    color: '#333',
+  image: {
+    width: 30,
+    height: 30,
+  },
+  buttonText: {
+    fontSize: 14,
+    color: '#000',
+    marginTop: 10, // Espaço entre o botão e o texto
   },
 });
 
 export default HomeScreen;
+
+
+
